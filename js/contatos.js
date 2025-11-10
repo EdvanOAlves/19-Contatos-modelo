a1ce10da66e36448114c43326e90c76cb05de0ca
EDVAN ALVES DE OLIVEIRA <25132698@CFP127ALUNO.LOCAL>
2025-11-10 08:58:42 -0300

    chore: init

---
+++ js/contatos.js
@@ -0,0 +1,74 @@
+export async function lerContatos() {
+    const url = "https://bakcend-fecaf-render.onrender.com/contatos"
+    const response = await fetch(url);
+    const contatos = await response.json();
+    console.log(contatos);
+    return contatos;
+}
+
+
+async function criarContato(contato) {
+    const url = "https://bakcend-fecaf-render.onrender.com/contatos"
+
+    const options = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(contato)
+    }
+
+    const response = await fetch(url, options);
+
+    console.log(response.ok);
+
+    return response.ok;
+}
+
+async function deletarContato(id) {
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
+
+    const options = {
+        method: "DELETE"
+    }
+
+    const response = await fetch(url, options);
+
+    return response.ok
+}
+
+async function atualizarContato(id, contato) {
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
+
+    const options = {
+        method: "PUT",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(contato)
+    }
+    const response = fetch(url, options)
+    console.log(response.ok);
+    return (await response).ok;
+}
+
+const novoContato = {
+    "nome": "Keys",
+    "celular": "n/a",
+    "foto": "https://i.pinimg.com/736x/98/62/99/98629953578bf8905079d4a2dfcc407f.jpg",
+    "email": "thekeysfromthe8@sbt.com",
+    "endereco": "vila, 8",
+    "cidade": "Acapulco"
+}
+
+const verdadeiroContato = {
+    "nome": "Edvan Alves",
+    "celular": "11 9 4002 8922",
+    "foto": "https://i.ebayimg.com/images/g/x6IAAOSwl0ZkZ9Ol/s-l1200.jpg",
+    "email": "edvansememailbrow@gmail.com",
+    "endereco": "Rua Rua"
+}
+
+criarContato(novoContato);
+// criarContato(verdadeiroContato);
+
---
+++ js/main.js
@@ -0,0 +1,7 @@
+'use strict'
+
+import {lerContatos} from "./contatos.js"
+
+
+const dados = lerContatos();
+
